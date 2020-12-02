a6dbcdaf5f03404847bd9f05d876e558ff13a5c5
Christian James Welly <chrisjwelly@gmail.com>
2020-12-02 10:29:33 +0800

    Add Milestone1 starter files

---
+++ Milestone1/Milestone1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone1
+//
+//  Created by Christian James Welly on 2/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
