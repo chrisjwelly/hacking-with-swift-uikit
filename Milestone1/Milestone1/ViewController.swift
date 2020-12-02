c1371d445834cb2b895c91987bf564124284944e
Christian James Welly <chrisjwelly@gmail.com>
2020-12-02 10:59:57 +0800

    Initialise TableView with Flag file names
    Note: Don't forget to change the identifier in the Storyboard for the
    TableView cell!

--- Milestone1/Milestone1/ViewController.swift
+++ Milestone1/Milestone1/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: Properties
     var pictures = [String]()
     
     override func viewDidLoad() {
@@ -26,6 +27,17 @@ class ViewController: UIViewController {
         }
         
         print(pictures)
+    }
+    
+    // MARK: TableView Controller
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
 
