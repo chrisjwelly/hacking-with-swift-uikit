0281cdd20583aec475c7dae323a88ebf5d016daa
Christian James Welly <chrisjwelly@gmail.com>
2020-12-02 10:53:24 +0800

    Add Flag assets under Bundle
    This is done through creating new Group under Milestone1, and then
    dragging all the image assets under this Group

--- Milestone1/Milestone1/ViewController.swift
+++ Milestone1/Milestone1/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if (item.hasSuffix("2x.png")) {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
 
