58dabd677c828b4f4f6918268ac512fad707c2f3
Christian James Welly <chrisjwelly@gmail.com>
2020-12-03 21:28:46 +0800

    Challenge 2: Use safeAreaLayoutGuide for leading and trailing

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -63,8 +63,8 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
             
             // label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
             
