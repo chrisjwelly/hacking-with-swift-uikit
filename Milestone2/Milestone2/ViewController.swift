859315a3ca836152231a39baa50d2c34b5419cca
Christian James Welly <chrisjwelly@gmail.com>
2020-12-04 13:38:47 +0800

    Add BarButton to add new items to the list
    This one required significantly more reference to Project 5. I was
    reminded of the `addTextField()` method, and perhaps I require some
    review of why a weak reference is needed in this context.

--- Milestone2/Milestone2/ViewController.swift
+++ Milestone2/Milestone2/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Shopping List"
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
     }
 
     // MARK: TableView Methods
@@ -27,6 +28,28 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
         cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
+    }
+    
+    // MARK: BarButtonItem Methods
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add new item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
 }
