298606051686878535908bbe0f76e78492bc9aef
Christian James Welly <chrisjwelly@gmail.com>
2020-12-04 13:30:01 +0800

    Initialise TableViewController for the Shopping List
    This one is pretty standard! I was able to do this without any
    reference, but I did forget to initialise the entry point of the
    ViewController.

--- Milestone2/Milestone2/ViewController.swift
+++ Milestone2/Milestone2/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: Properties
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Shopping List"
         // Do any additional setup after loading the view.
     }
 
+    // MARK: TableView Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
 
 }
 
