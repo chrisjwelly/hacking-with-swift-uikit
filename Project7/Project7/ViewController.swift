612bbf94f05fc929169ff451f02341d4f5bc5dfb
Christian James Welly <chrisjwelly@gmail.com>
2020-12-04 20:59:46 +0800

    Add Project7 starter files

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Christian James Welly on 4/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
