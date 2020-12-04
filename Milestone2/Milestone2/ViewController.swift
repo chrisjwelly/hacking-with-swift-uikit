22abf60b2ea93be52899ebb439b75194005d1bc4
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-12-04 14:37:52 +0800

    Complete Milestone 2: Projects 4 - 6 (#7)
    * Add Milestone2 starter files
    
    * Initialise TableViewController for the Shopping List
    
    This one is pretty standard! I was able to do this without any
    reference, but I did forget to initialise the entry point of the
    ViewController.
    
    * Add BarButton to add new items to the list
    
    This one required significantly more reference to Project 5. I was
    reminded of the `addTextField()` method, and perhaps I require some
    review of why a weak reference is needed in this context.
    
    * Add button to clear the shopping list
    
    To create the handler for the confirm UIAlertAction, I tried using
    closures. Initially I just used a single parameter `_`, and tried
    accessing `shoppingList` as well as `tableView` directly in the closure.
    However it gave me an error.
    
    The error is along the lines of: "Reference to property in closure
    requires explicit use of 'self' to make capture semantics explicit."
    This was when I opted to use the capture list with a weak reference to
    self. But again, I used weak reference because this is how it was done
    all along.
    
    Additional note: I wonder how different it is to call the `removeAll()`
    method and assigning a new empty array to the shoppingList array.
    
    * Prepare ActivityViewController for sharing
    
    Here, I discovered the `rightBarButtonItems` property of
    UINavigationItem. This allows multiple BarButtonItems instead of a
    single one. I initially wanted to use a toolbar for the share, but this simplifies things a lot!
    
    A small challenge I faced is in the handling of the
    popoverPresentationController for iPad. I usually set it to the
    rightBarButtonItem, but that will cause it to pop-up to the right-most
    item. The share button is the second right-most, so I need to index the
    array.
    
    Currently, the index is hardcoded as I know exactly where it is.
    However, perhaps a better way would be to have some properties that
    dictates the position of such BarButtonItems in the array. That way, if
    I ever feel like swapping the position of the share button, I can simply
    change the index in the array.
    
    An alternative I am thinking is to find the indexOf the button that I
    want in the Array. I think this is tricky to do because it requires some
    understanding of the equality of the UI BarButtonItem
    
    * Join strings in shoppingList that can be used for sharing
    
    Looks like you can't share arrays in activityItems!
    
    * Add cancel button when trying to add for slightly better UX
    
    * Prevent empty strings from being added to the list

---
+++ Milestone2/Milestone2/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Milestone2
+//
+//  Created by Christian James Welly on 4/12/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    // MARK: Properties
+    var shoppingList = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Shopping List"
+        
+        // Creating BarButtonItems
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        let clearButton = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(clearShoppingList))
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareShoppingList))
+        
+        // Setting the BarButtonItems
+        navigationItem.rightBarButtonItems = [addButton, shareButton]
+        navigationItem.leftBarButtonItem = clearButton
+        
+    }
+
+    // MARK: TableView Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    // MARK: BarButtonItem Methods
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add new item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        if item == "" {
+            showErrorMessage(errorTitle: "Item cannot be empty!", errorMessage: "Try typing something!")
+        } else {
+            shoppingList.insert(item, at: 0)
+            
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+    @objc func clearShoppingList() {
+        let ac = UIAlertController(title: "Are you sure?", message: "This will clear your list. It cannot be undone.", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) {
+            [weak self] _ in
+            self?.shoppingList.removeAll()
+            self?.tableView.reloadData()
+        }
+        
+        ac.addAction(confirmAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    @objc func shareShoppingList() {
+        let joinedShoppingList = shoppingList.joined(separator: "\n")
+        let vc = UIActivityViewController(activityItems: [joinedShoppingList], applicationActivities: [])
+        
+        // Hardcoded because the project is still pretty small. Ideally the index is shared
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItems?[1]
+        
+        present(vc, animated: true)
+    }
+    
+    // MARK: Error Message
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Close", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+}
+
