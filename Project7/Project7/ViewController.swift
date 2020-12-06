49b234667022043c7387bf41280b1ffaff782bde
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-12-06 09:58:02 +0800

    Complete Project 7 (#8)
    * Add Project7 starter files
    
    * Add basic Project7
    
    Note that the changes to AppDelegate should be made to SceneDelegate
    instead
    
    * Challenge 1: Add Credits button with UIBarButtonItem
    
    Pretty standard!
    
    * Challenge 2: Prepare additional filtered array and states
    
    I expect it to be some Boolean checks. Once I click on filter, it will
    switch state to filtered state and only show the contents of the filteredPetitions
    
    * Challenge 2: Add method to fill in the filteredPetitions
    
    This requires changing the filtered flag as well as reloading the
    tableView
    
    * Challenge 2: Add method to clear filter
    
    I got a bit lazy in resetting the button to the original filter button,
    so I reused the old method of trying to configure the navbar. I probably
    should abstract it a bit so that it can be easily reused.
    
    * Challenge 2: Refactor methods to change the LeftBarButtonItem
    
    * Challenge 2: Include filter keyword in the title of NavBar
    
    However, this won't work for very long filter keywords. Will probably
    need another way to show this to the user.
    
    * Update README for Project7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,173 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Christian James Welly on 4/12/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    enum LeftBar {
+        case filter, clearFilter
+    }
+    
+    // MARK: Properties
+    var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var filtered = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+        
+        print("didLoad")
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        configureNavBar()
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                // We're OK to parse
+                parse(json:data)
+                return
+                
+            }
+        }
+        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: Navbar methods
+    func configureNavBar() {
+        configureRightBar()
+        configureLeftBar(mode: .filter)
+    }
+    
+    func configureRightBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+    }
+    
+    func configureLeftBar(mode: LeftBar) {
+        switch mode {
+        case .filter:
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filter))
+        case .clearFilter:
+            navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear Filter", style: .plain, target: self, action: #selector(clearFilter))
+        }
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The information provided here is from the We The People API of the Whitehouse", preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "Close", style: .default)
+        ac.addAction(closeAction)
+        
+        present(ac, animated: true)
+    }
+    
+    @objc func filter() {
+        let ac = UIAlertController(title: "Enter filter keyword", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let filterKeyword = ac?.textFields?[0].text else { return }
+            self?.submit(filterKeyword)
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ filterKeyword: String) {
+        for petition in petitions {
+            let title = petition.title
+            let body = petition.body
+            
+            if (title.contains(filterKeyword) || body.contains(filterKeyword)) {
+                filteredPetitions.append(petition)
+            }
+        }
+        
+        filtered = true
+        title = "Filtering for: " + filterKeyword
+        tableView.reloadData()
+        
+        configureLeftBar(mode: .clearFilter)
+    }
+    
+    @objc func clearFilter() {
+        let ac = UIAlertController(title: "Are you sure?", message: "This will reset your filters.", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) {
+            [weak self] _ in
+            
+            self?.configureLeftBar(mode: .filter)
+
+            self?.filtered = false
+            self?.title = nil
+            self?.tableView.reloadData()
+        }
+        ac.addAction(confirmAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    // MARK: TableView Methods
+    func getCurrPetitions() -> [Petition] {
+        if (filtered) {
+            return filteredPetitions
+        } else {
+            return petitions
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let currPetitions = getCurrPetitions()
+        return currPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currPetitions = getCurrPetitions()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = currPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currPetitions = getCurrPetitions()
+        let vc = DetailViewController()
+        vc.detailItem = currPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+}
+
